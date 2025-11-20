3c3e917a3fdcf8e9b9c870c9749effff4a7d200f
lklostermair <lukas.klostermair@gmail.com>
2025-11-20 09:08:05 -0800

    small fixes and files

---
+++ test.cpp
@@ -0,0 +1,12 @@
+// test for c++ programming
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter Number!" << endl;
+
+    cin >> a;
+    cout << "User entered number " << a << endl;
+}
